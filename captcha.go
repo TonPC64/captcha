8d011ea6b420b6e58b75fdfa26219d47a0bde45c
TonPC64 <kingkong2103@gmail.com>
2019-01-05 15:56:02 +0700

    refactor func joinWithSpace

--- captcha.go
+++ captcha.go
@@ -5,22 +5,24 @@ import (
 	"strings"
 )
 
-var operator = [3]string{"+", "-", "x"}
-var operand = [9]string{
-	"ONE",
-	"TWO",
-	"THREE",
-	"FOUR",
-	"FIVE",
-	"SIX",
-	"SEVEN",
-	"EIGHT",
-	"NINE",
-}
+var (
+	operator = [3]string{"+", "-", "x"}
+	operand  = [9]string{
+		"ONE",
+		"TWO",
+		"THREE",
+		"FOUR",
+		"FIVE",
+		"SIX",
+		"SEVEN",
+		"EIGHT",
+		"NINE",
+	}
+)
 
 // Captcha is function create captcha
-func Captcha(pat, n1, op, n2 int) string {
-	if pat < 1 || pat > 2 {
+func Captcha(pattern, n1, op, n2 int) string {
+	if pattern < 1 || pattern > 2 {
 		return ""
 	}
 	if op < 1 || op > 3 {
@@ -30,20 +32,16 @@ func Captcha(pat, n1, op, n2 int) string {
 		return ""
 	}
 
-	if pat == 1 {
-		return joinString(strconv.Itoa(n1), operator[op-1], operand[n2-1])
-	} else if pat == 2 {
-		return joinString(operand[n2-1], operator[op-1], strconv.Itoa(n1))
+	if pattern == 1 {
+		return joinWithSpace(strconv.Itoa(n1), operator[op-1], operand[n2-1])
+	} else if pattern == 2 {
+		return joinWithSpace(operand[n2-1], operator[op-1], strconv.Itoa(n1))
 	}
 	return ""
 }
 
-func joinString(s ...string) string {
-	res := []string{}
-	for _, val := range s {
-		res = append(res, val)
-	}
-	return strings.Join(res, " ")
+func joinWithSpace(s ...string) string {
+	return strings.Join(s, " ")
 }
 
 func validateNumber(n int) bool {
